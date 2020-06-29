23ec39d6f83382f231f73a632a4a692a50b885ea
Idan <idan.zonshein@powerhrg.com>
2020-06-29 15:16:45 -0400

    Add total distance and par to course model and views

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,12 @@ class Course < ApplicationRecord
   has_many :visits
   has_many :users, through: :visits
   has_many :holes
+
+  def total_par
+    holes.map(&:par_score).sum
+  end
+
+  def total_distance
+    holes.map(&:distance).sum
+  end
 end
