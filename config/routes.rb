4495c1c2f337a2a564d21be6b30e0222573fa4d9
Idan <idan.zonshein@powerhrg.com>
2020-06-30 11:54:49 -0400

    Add all users index page and user show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :holes
     resources :visits
   end
+  resources :users, only: [:index, :show]
+
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
