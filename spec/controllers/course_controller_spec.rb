f219449281b55dcb1cccbba66fac49306dee8327
Idan <idan.zonshein@powerhrg.com>
2020-07-01 14:32:41 -0400

    Finish integration testing

--- spec/controllers/course_controller_spec.rb
+++ spec/controllers/course_controller_spec.rb
@@ -1,3 +1,20 @@
 require "rails_helper"
 
-RSpec.describe CoursesController do
+RSpec.describe CoursesController, type: :controller do
+  describe "GET index" do
+    it "assigns @courses" do
+      user = create :user
+      sign_in(user)
+      course = create :course
+      get :index
+      expect(assigns(:courses)).to eq([course])
+    end
+
+    it "renders the index template" do
+      user = create :user
+      sign_in(user)
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+end
