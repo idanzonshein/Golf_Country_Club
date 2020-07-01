e42288a2754724ec9580b73f00b649bc2401bacd
Idan <idan.zonshein@powerhrg.com>
2020-07-01 08:16:58 -0400

    Add validations to hole model

--- app/models/hole.rb
+++ app/models/hole.rb
@@ -1,5 +1,23 @@
 class Hole < ApplicationRecord
   belongs_to :course
 
+  validates :valid_hole
+  validates :valid_par
+  validates :hole_number,
+            uniqueness: { scope: :course_id,
+                          message: "A basket number must be unique." }
+
   scope :by_number, -> { order(:hole_number) }
+
+private
+
+  def valid_hole
+    errors.add(:hole_number, "Hole must be greater than 0") \
+    if hole_number.nil? || hole_number <= 0
+  end
+
+  def valid_par
+    errors.add(:par_score, "Par Score Is Never Less Than 3") \
+    if par_score.nil? || par_score <= 3
+  end
 end
